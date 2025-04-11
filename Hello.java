93e1716c0573cd62fd3123fc210658b4c4da9212
darksunnnnn <kaidecookie@gmail.com>
2025-04-11 19:20:32 +0000

    Modified hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,17 @@
 public class Hello{
 
     public static void main(String[] args){
-        System.out.println("Hello World");
+       String name = getName();
+       greet(name);
     }
-
+        
+public static void greet(String name){
+        System.out.println("Hello " + name + "!");
+    }
+        public static String getName(){
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        String name = scanner.nextLine();
+        scanner.close();
+        return name;
+    }
 }
